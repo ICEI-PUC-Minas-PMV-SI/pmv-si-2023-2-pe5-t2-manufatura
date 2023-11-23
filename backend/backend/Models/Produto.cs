296c129af1ba3f04d4f23116b6963870eda4fc91
Carolina Moura <crvo.carolina@gmail.com>
2023-11-23 20:58:47 -0300

    Add model Produto

---
+++ backend/backend/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a cor")]
+        public string Cor { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o preço")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a quantidade em estoque")]
+        public int QuantidadeEstoque { get; set; }
+
+    }
+}
